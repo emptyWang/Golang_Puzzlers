1814cba12dcb071bf12ef6583b51c8a32384c8d5
wangzezhong <wangzezhong@zuoyebang.com>
2021-04-06 23:52:29 +0800

    test init sequence with var init fun and main func

--- src/puzzlers/article2/q3/demo31.go
+++ src/puzzlers/article2/q3/demo31.go
@@ -6,7 +6,17 @@ import (
 	"os"
 )
 
+//first init var
+//second init func
+//last main func
+
 var nickName string
+var T = a()
+
+func a() int64 {
+	fmt.Println("calling a()")
+	return 1
+}
 
 func init() {
 	flag.CommandLine = flag.NewFlagSet("", flag.ExitOnError)
@@ -14,6 +24,7 @@ func init() {
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
 		flag.PrintDefaults()
 	}
+	fmt.Println(T)
 	flag.StringVar(&nickName, "nickName", "everyone", "The greeting object.")
 }
 
