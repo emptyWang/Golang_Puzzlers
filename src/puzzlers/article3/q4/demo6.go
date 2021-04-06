8fd522cfdae95d7c1ead76c784670a0fbd4afd42
wangzezhong <wangzezhong@zuoyebang.com>
2021-04-06 23:04:17 +0800

    adjust directory

--- src/puzzlers/article3/q4/demo6.go
+++ src/puzzlers/article3/q4/demo6.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"puzzlers/article3/q4/lib"
-	//in "puzzlers/article3/q4/lib/internal" // 此行无法通过编译。
+	"puzzlers/src/puzzlers/article3/q4/lib"
+	//in "puzzlers/src/puzzlers/article3/q4/lib/internal" // 此行无法通过编译。
 	//"os"
 )
 
